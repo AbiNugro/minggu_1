5178eb5ee49efa016621e28c50d1e595ee4cf32d
AbiNugro <abinugroho2901@gmail.com>
2025-08-27 08:46:50 +0700

    Tugas minggu 1

---
+++ minggu_1.php
@@ -0,0 +1,21 @@
+<?php
+
+$x = 2;
+
+echo "<br/>tugas pertama <br/><br/>";
+
+for($i = 1; $i <= 10; $i++) {
+    echo $x . ' ';
+    $x = $x * 2;
+}
+
+
+echo "<br/><br/>tugas kedua<br/><br/>";
+$y = 12;
+
+for($j = 1; $j <= 5; $j++) {
+    echo $y . ' ';
+    $y = $y * 2;
+    echo $y . ' ';
+    $y = $y + 1;
+}
